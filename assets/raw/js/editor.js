1d58d41648ea967edd5fc9043a46b5f847aaf865
Griffin Bishop <grbishop@wpi.edu>
2017-10-27 17:33:49 +0200

    Editor changes

--- assets/raw/js/editor.js
+++ assets/raw/js/editor.js
@@ -3,6 +3,11 @@ import update_breadcrumbs from './utils';
 function render_editor(route){
   update_breadcrumbs(route);
 
+  const visual = document.createElement('div');
+  visual.className = 'visual';
+
+  const page = document.getElementById('page');
+  page.appendChild(visual);
 }
 
 export default render_editor;
