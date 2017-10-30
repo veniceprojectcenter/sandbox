4249b8d1676895713cce40ef53738e876a5e8f95
Griffin Bishop <grbishop@wpi.edu>
2017-10-30 15:39:22 +0100

    Breadcrumbs update, bad css though D:

--- assets/raw/js/utils.js
+++ assets/raw/js/utils.js
@@ -2,8 +2,13 @@ function updateBreadcrumbs(route) {
   const breadcrumbs = document.getElementById('breadcrumbs');
   let breadcrumbString = '';
   for (let i = 0; i < route.length; i += 1) {
-    breadcrumbString += `&nbsp>&nbsp${route[i]}`;
+    if (i !== 0) {
+      breadcrumbString += `&nbsp>&nbsp${route[i]}`;
+    } else {
+      breadcrumbString += `&nbsp>&nbsp<a href="/${route[i]}/">${route[i]}</a>`;
+    }
   }
+
   breadcrumbs.innerHTML = breadcrumbString;
 }
 
