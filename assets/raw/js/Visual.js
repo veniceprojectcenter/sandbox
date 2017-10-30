30073e6960c145e3079a9f558c943cdc87af6005
Myles Spencer <mspencer.mes@gmail.com>
2017-10-30 06:29:35 -0400

    Added renderControl method to Visuals.js

--- assets/raw/js/Visual.js
+++ assets/raw/js/Visual.js
@@ -11,6 +11,10 @@ class Visual {
     return [{ data: 'This needs to be changed', name: dataName }];
   }
 
+  renderControls() {
+    throw new Error('You must implement this method');
+  }
+
   render() {
     throw new Error('You must implement this method');
   }
