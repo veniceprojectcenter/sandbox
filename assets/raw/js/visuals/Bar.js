5dc5880b2a7b158cac51ffec5c90a1bb8b0d447e
Jessie Ying <jessie.ying@gmail.com>
2017-10-30 17:33:44 +0100

    bar chart works

--- assets/raw/js/visuals/Bar.js
+++ assets/raw/js/visuals/Bar.js
@@ -4,8 +4,8 @@ class Bar extends Visual {
   constructor(config) {
     super(config);
     this.applyDefaultAttributes({
-      width: 960,
-      height: 500,
+      width: 600,
+      height: 400,
       font_size: '1em',
       colors: [],
       category_order: '',
@@ -13,45 +13,57 @@ class Bar extends Visual {
   }
 
   render() {
+    // console.log(this.data);
+    const margin = { top: 10, right: 10, bottom: 20, left: 20 };
+    const width = this.attributes.width - margin.left - margin.right;
+    const height = this.attributes.height - margin.top - margin.bottom;
 
     const svg = d3.select(`#${this.renderID}`).append('svg')
-        margin = {top: 20, right: 20, bottom: 30, left: 40},
-        width = this.attributes.width - margin.left - margin.right,
-        height = this.attributes.height - margin.top - margin.bottom;
+      .attr('width', width)
+      .attr('height', height)
+      .attr('class', 'bar')
+      .attr('viewBox', '0 0 600 400')
+      .append('g');
 
-    let x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
-        y = d3.scaleLinear().rangeRound([height, 0]);
+    const x = d3.scaleBand().rangeRound([0, width]).padding(0.1);
+    const y = d3.scaleLinear().rangeRound([height, 0]);
 
-    let g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    const g = svg.append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`);
 
-    x.domain(this.data.map(function(d) { return d.key; }));
-    y.domain([0, d3.max(this.data, function(d) { return d.value; })]);
+    // d3.json(this.data, (d) => {
+    //   d.height = +d.height;
+    //   return d;
+    // }, (error, data) => {
+    //   if (error) throw error;
 
-    g.append("g")
-    .attr("class", "axis axis--x")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
+    x.domain(this.data.map(d => d.id));
+    y.domain([0, d3.max(this.data, d => d.height)]);
 
-    g.append("g")
-      .attr("class", "axis axis--y")
-      .call(d3.axisLeft(y).ticks(10, "%"))
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", "0.71em")
-      .attr("text-anchor", "end")
-      .text("value");
+    g.append('g')
+      .attr('class', 'axis axis--x')
+      .attr('transform', `translate(0,${height})`)
+      .call(d3.axisBottom(x));
 
-    g.selectAll(".bar")
-      .data(this.data)
-      .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", function(d) { return x(d.key); })
-        .attr("y", function(d) { return y(d.value); })
-        .attr("width", x.bandwidth())
-        .attr("height", function(d) { return height - y(d.value); });
+    g.append('g')
+        .attr('class', 'axis axis--y')
+        .call(d3.axisLeft(y).ticks(10))
+      .append('text')
+        .attr('transform', 'rotate(-90)')
+        .attr('y', 6)
+        .attr('dy', '0.71em')
+        .attr('text-anchor', 'end')
+        .text('height');
 
+    g.selectAll('.bar')
+        .data(this.data)
+        .enter().append('rect')
+          .attr('class', 'bar')
+          .attr('x', d => x(d.id))
+          .attr('y', d => y(d.height))
+          .attr('width', x.bandwidth())
+          .attr('height', d => height - y(d.height));
+    // });
   }
 }
 
