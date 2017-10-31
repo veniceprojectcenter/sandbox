9b03ac6babb61e67bd97ebabcd3f79bc3f8d29ec
co1in <colinjw3@gmail.com>
2017-10-31 11:57:36 +0100

    Reverted materialize removal

--- assets/raw/js/visuals/ComponentUtils.js
+++ assets/raw/js/visuals/ComponentUtils.js
@@ -4,14 +4,13 @@ class ComponentUtils {
   static createTextField(container, id, title, onTextChanged) {
     const context = { id, title };
     ComponentUtils.handlebarsWithContext(container, 'text-field-entry', context);
-
-    document.querySelector(`#${id} > input`).addEventListener('input', onTextChanged);
+    $(`#${id} > input`).on('input', onTextChanged);
   }
 
   static createSelectBox(container, id, title, options, onOptionChanged) {
     const context = { id, title, options };
     ComponentUtils.handlebarsWithContext(container, 'select-entry', context);
-    document.querySelector(`#${id} > select`).addEventListener('change', onOptionChanged);
+    $(`#${id} > select`).change(onOptionChanged).material_select();
     console.log(document.querySelector(`#${id} > select`));
   }
 
@@ -25,8 +24,10 @@ class ComponentUtils {
     const source = document.getElementById(handlebarId).innerHTML;
     const template = Handlebars.compile(source);
     const html = template(context);
+
     container.innerHTML += html;
   }
+
 }
 
 export default ComponentUtils;
