84fb8c43f6a0522d76ca3fbfe01b2d2b70bddd20
Griffin Bishop <grbishop@wpi.edu>
2017-10-31 12:31:44 +0100

    Added PieChartMap to editor

---
+++ assets/raw/js/visuals/PieChartMap.js
@@ -0,0 +1,317 @@
+import Visual from '../Visual';
+
+class PieChartMap extends Visual {
+  constructor(config) {
+    super(config);
+
+    this.map = null;
+    this.locations = [];
+    this.openInfoWindow = null;
+  }
+
+  static determineColor(year) {
+    if (year) {
+      let range = parseFloat(year) - 1500;
+      if (range > 255) {
+        range = 255;
+      }
+      return `#${range.toString(16)}${(255 - range).toString(16)}FF`;
+    }
+    return '#FF0000';
+  }
+
+  addMarker(data) {
+    if (data.lat && data.lng) {
+      const icon = {
+        path: 'M-20,0a5,5 0 1,0 10,0a5,5 0 1,0 -10,0',
+        fillColor: Map.determineColor(data.approximate_year),
+        fillOpacity: 0.6,
+        anchor: new google.maps.Point(0, 0),
+        strokeWeight: 0,
+        scale: 1,
+      };
+      const marker = new google.maps.Marker({
+        position: {
+          lat: parseFloat(data.lat),
+          lng: parseFloat(data.lng),
+        },
+        map: this.map,
+        title: data.wiki_friendly_title,
+        animation: google.maps.Animation.DROP,
+        icon,
+      });
+
+      let contentString = `${'<div id="content">' +
+            '<div id="siteNotice">' +
+            '</div>' +
+            '<h1 id="firstHeading" class="firstHeading">'}${data.wiki_friendly_title}</h1>` +
+            '<div id="bodyContent">';
+
+      if (data.description_italian) {
+        contentString += `<p><b>Description: </b>${data.description_italian}</p>`;
+      }
+
+      if (data.approximate_year) {
+        contentString += `<p><b>Approximate Year: </b>${data.approximate_year}</p>`;
+      }
+
+      if (data.risk_factor_metal_description) {
+        contentString += `<p><b>Risk Factor: </b> ${data.risk_factor_metal_description}</p>`;
+      }
+
+      contentString += '</div></div>';
+
+      const infowindow = new google.maps.InfoWindow({
+        content: contentString,
+      });
+
+      marker.addListener('click', () => {
+        if (this.openInfoWindow) {
+          this.openInfoWindow.close();
+        }
+        infowindow.open(this.map, marker);
+        this.openInfoWindow = infowindow;
+      });
+
+      this.locations.push({
+        marker,
+        infowindow,
+      });
+    }
+  }
+
+  render() {
+    this.map = new google.maps.Map(document.getElementById(this.renderID), {
+      center: { lat: 45.43, lng: 12.33 },
+      zoom: 13,
+      styles: [
+        {
+          elementType: 'geometry',
+          stylers: [
+            {
+              color: '#f5f5f5',
+            },
+          ],
+        },
+        {
+          elementType: 'labels',
+          stylers: [
+            {
+              visibility: 'off',
+            },
+          ],
+        },
+        {
+          elementType: 'labels.icon',
+          stylers: [
+            {
+              visibility: 'off',
+            },
+          ],
+        },
+        {
+          elementType: 'labels.text.fill',
+          stylers: [
+            {
+              color: '#616161',
+            },
+          ],
+        },
+        {
+          elementType: 'labels.text.stroke',
+          stylers: [
+            {
+              color: '#f5f5f5',
+            },
+          ],
+        },
+        {
+          featureType: 'administrative',
+          elementType: 'geometry',
+          stylers: [
+            {
+              visibility: 'off',
+            },
+          ],
+        },
+        {
+          featureType: 'administrative.land_parcel',
+          stylers: [
+            {
+              visibility: 'off',
+            },
+          ],
+        },
+        {
+          featureType: 'administrative.land_parcel',
+          elementType: 'labels.text.fill',
+          stylers: [
+            {
+              color: '#bdbdbd',
+            },
+          ],
+        },
+        {
+          featureType: 'administrative.neighborhood',
+          stylers: [
+            {
+              visibility: 'off',
+            },
+          ],
+        },
+        {
+          featureType: 'poi',
+          stylers: [
+            {
+              visibility: 'off',
+            },
+          ],
+        },
+        {
+          featureType: 'poi',
+          elementType: 'geometry',
+          stylers: [
+            {
+              color: '#eeeeee',
+            },
+          ],
+        },
+        {
+          featureType: 'poi',
+          elementType: 'labels.text.fill',
+          stylers: [
+            {
+              color: '#757575',
+            },
+          ],
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'geometry',
+          stylers: [
+            {
+              color: '#e5e5e5',
+            },
+          ],
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'labels.text.fill',
+          stylers: [
+            {
+              color: '#9e9e9e',
+            },
+          ],
+        },
+        {
+          featureType: 'road',
+          stylers: [
+            {
+              visibility: 'off',
+            },
+          ],
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry',
+          stylers: [
+            {
+              color: '#ffffff',
+            },
+          ],
+        },
+        {
+          featureType: 'road',
+          elementType: 'labels.icon',
+          stylers: [
+            {
+              visibility: 'off',
+            },
+          ],
+        },
+        {
+          featureType: 'road.arterial',
+          elementType: 'labels.text.fill',
+          stylers: [
+            {
+              color: '#757575',
+            },
+          ],
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry',
+          stylers: [
+            {
+              color: '#dadada',
+            },
+          ],
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'labels.text.fill',
+          stylers: [
+            {
+              color: '#616161',
+            },
+          ],
+        },
+        {
+          featureType: 'road.local',
+          elementType: 'labels.text.fill',
+          stylers: [
+            {
+              color: '#9e9e9e',
+            },
+          ],
+        },
+        {
+          featureType: 'transit',
+          stylers: [
+            {
+              visibility: 'off',
+            },
+          ],
+        },
+        {
+          featureType: 'transit.line',
+          elementType: 'geometry',
+          stylers: [
+            {
+              color: '#e5e5e5',
+            },
+          ],
+        },
+        {
+          featureType: 'transit.station',
+          elementType: 'geometry',
+          stylers: [
+            {
+              color: '#eeeeee',
+            },
+          ],
+        },
+        {
+          featureType: 'water',
+          elementType: 'geometry',
+          stylers: [
+            {
+              color: '#c9c9c9',
+            },
+          ],
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.fill',
+          stylers: [
+            {
+              color: '#9e9e9e',
+            },
+          ],
+        },
+      ],
+    });
+  }
+}
+
+export default PieChartMap;
