0a7fa49ce30a176924aba55fae9cc2d8cf3d0cf6
Myles Spencer <mspencer.mes@gmail.com>
2017-10-31 11:30:42 -0400

    Added base firebase code

--- assets/raw/js/routing.js
+++ assets/raw/js/routing.js
@@ -27,4 +27,16 @@ function routing() {
   }
 }
 
+
+// Initialize Firebase - might want to move to a different place eventually
+const config = {
+  apiKey: 'AIzaSyBqpAQE7wrvXnitoSwso-QTDa_5U8fHvgM',
+  authDomain: 'vpc-sandbox-test.firebaseapp.com',
+  databaseURL: 'https://vpc-sandbox-test.firebaseio.com',
+  projectId: 'vpc-sandbox-test',
+  storageBucket: 'vpc-sandbox-test.appspot.com',
+  messagingSenderId: '962614899759',
+};
+firebase.initializeApp(config);
+
 routing();
