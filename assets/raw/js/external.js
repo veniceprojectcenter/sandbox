3b549a812a81880628ff86a7788cffef3e75894d
Myles Spencer <mspencer.mes@gmail.com>
2017-11-07 07:58:23 -0500

    Added support for external use of visual classes

---
+++ assets/raw/js/external.js
@@ -0,0 +1,14 @@
+import Visual from './Visual';
+import Map from './visuals/Map';
+import Donut from './visuals/Donut';
+import Bar from './visuals/Bar';
+import Counter from './visuals/Counter';
+import PieChartMap from './visuals/PieChartMap';
+
+window.visualize = {};
+window.visualize.Visual = Visual;
+window.visualize.Map = Map;
+window.visualize.Donut = Donut;
+window.visualize.Bar = Bar;
+window.visualize.Counter = Counter;
+window.visualize.PieChartMap = PieChartMap;
