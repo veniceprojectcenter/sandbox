af1d999cfac2f3e3be3d5c2fefc4ffc2f4e7e658
Griffin Bishop <grbishop@wpi.edu>
2017-11-20 12:54:22 +0100

    Created new Map basis class

---
+++ assets/raw/js/visuals/helpers/Map.js
@@ -0,0 +1,124 @@
+import Visual from './Visual';
+import DefaultMapStyle from './DefaultMapStyle';
+
+/* This file is to be used as a default starting point for new map visualizations
+ * that feature adding divs
+*/
+
+class Map extends Visual {
+  constructor(config) {
+    super(config);
+
+    this.map = null;
+    this.locations = [];
+    this.openInfoWindow = null;
+  }
+
+  onLoadData() {
+    this.applyDefaultAttributes({
+      title: '',
+    });
+
+    const columnNames = Object.keys(this.data[0]);
+    const selections = [columnNames[52]];
+    const groups = Visual.groupByMultiple(selections, this.data);
+
+    console.log(groups);
+  }
+
+  render() {
+    this.map = new google.maps.Map(document.getElementById(this.renderID), {
+      center: { lat: 45.435, lng: 12.335 },
+      zoom: 14,
+      styles: DefaultMapStyle,
+    });
+
+    // this.registerDefaultClickAction();
+  }
+
+  registerDefaultClickAction() {
+    google.maps.event.addListener(this.map, 'click', (event) => {
+      console.log(`Lat: ${event.latLng.lat()}| Lng: ${event.latLng.lng()}`);
+      this.addMarker(event.latLng.lat(), event.latLng.lng());
+      this.addMarker(event.latLng.lat(), event.latLng.lng());
+
+      const bounds = new google.maps.LatLngBounds(
+       new google.maps.LatLng(event.latLng.lat(),
+                              event.latLng.lng()),
+       new google.maps.LatLng(event.latLng.lat() + 0.01,
+                              event.latLng.lng() + 0.01),
+      );
+
+      const renderfunction = (id) => {
+        const config = {
+          dataSet: this.dataSet,
+          type: 'donut',
+          attributes: {},
+        };
+
+        const donutVisual = new Donut(config);
+        donutVisual.loadStaticData(this.data);
+        donutVisual.renderID = id;
+        donutVisual.render();
+      };
+
+      if (this.currentId == null) {
+        this.currentId = 1;
+      } else {
+        this.currentId += 1;
+      }
+
+      new DivOverlay(bounds, `overlay${this.currentId}`, this.map, renderfunction);
+    });
+  }
+
+  addMarker(lat, lng) {
+    if (lat && lng) {
+      const icon = {
+        path: 'M-20,0a5,5 0 1,0 10,0a5,5 0 1,0 -10,0',
+        fillColor: 'blue',
+        fillOpacity: 0.6,
+        anchor: new google.maps.Point(0, 0),
+        strokeWeight: 0,
+        scale: 1,
+      };
+      const marker = new google.maps.Marker({
+        position: {
+          lat: parseFloat(lat),
+          lng: parseFloat(lng),
+        },
+        map: this.map,
+        title: '',
+        animation: google.maps.Animation.DROP,
+        icon,
+      });
+
+      this.locations.push({
+        marker,
+      });
+    }
+  }
+
+  clearMarkers() {
+    this.locations.forEach((marker) => {
+      marker.setMap(null);
+    });
+    this.locations = [];
+  }
+
+  renderControls() {
+    if (this.data.length === 0) {
+      alert('Dataset is empty!');
+      return;
+    }
+
+    Visual.empty(this.renderControlsID);
+    const controlsContainer = document.getElementById(this.renderControlsID);
+
+    const editor = new EditorGenerator(controlsContainer);
+
+    editor.createHeader('Editor');
+  }
+}
+
+export default DefaultMap;
