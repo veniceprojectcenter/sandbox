91272d2814fd7dc68ff4b8b9b840162ebeb8cc14
Drew Ciccarelli <dcicc4@comcast.net>
2017-12-01 09:34:09 -0500

    DataView+Counter done, color only shows in exported SVG though

--- assets/raw/js/visuals/Counter.js
+++ assets/raw/js/visuals/Counter.js
@@ -7,11 +7,8 @@ import Filter from './helpers/Filter';
 class Counter extends Visual {
   constructor(config, renderID, renderControlsID) {
     super(config, renderID, renderControlsID);
-
     this.renderData = [];
-    this.attributes.filters = [];
     this.filter = new Filter(this);
-
     this.num = 0;
     this.applyDefaultAttributes({
       width: 500,
@@ -28,12 +25,14 @@ class Counter extends Visual {
   *
   */
   renderControls() {
+    this.attributes.filters = [];
     this.attributes.aggs = [];
     this.attributes.columnOptions = null;
     this.attributes.displayColumns = [];
     this.attributes.dataFilters = [];
     this.attributes.numericFilters = [];
     this.attributes.fontSize = 32;
+    this.attributes.color = '#000000';
     this.renderData = JSON.parse(JSON.stringify(this.data));
     this.attributes.columnOptions = Object.keys(this.data[0]);
     this.renderControlsDiv = document.getElementById(this.renderControlsID);
@@ -46,13 +45,16 @@ class Counter extends Visual {
     const editor = new EditorGenerator(this.renderControlsDiv);
     const aggDiv = document.createElement('div');
     this.renderControlsDiv.appendChild(aggDiv);
-    Counter.createAggregationRow(aggDiv, this.cats, 0);
-    this.createAddAggregation(this.renderControlsDiv, aggDiv);
+    Counter.createAggregationRow(aggDiv, this.cats);
     const br = document.createElement('br');
     this.renderControlsDiv.appendChild(br);
     br.style.margin = '10px';
     editor.createNumberSlider('fontSlider', 'Font Size', this.attributes.fontSize, 1, 128, (e) => {
       this.attributes.fontSize = $(e.currentTarget).val();
+      this.render();
+    });
+    editor.createColorField('color', 'Color', '#000000', (e) => {
+      this.attributes.color = $(e.currentTarget).val();
       this.render();
     });
     const myDiv = document.createElement('div');
@@ -64,29 +66,32 @@ class Counter extends Visual {
   *
   */
   render() {
-    this.filter.getFilteredData(this.attributes.filters);
-    if (this.renderData.length > 0) {
-      this.renderData = this.renderData[0];
-    }
-    this.renderDiv = document.getElementById(this.renderID);
-    this.renderDiv.innerHTML = '';
-    const svg = document.createElement('svg');
-    this.renderDiv.appendChild(svg);
-    const g = document.createElement('g');
+    if (this.attributes.filters !== undefined) {
+      this.filter.getFilteredData(this.attributes.filters);
+      if (this.renderData.length > 0) {
+        this.renderData = this.renderData[0];
+      }
+      this.renderDiv = document.getElementById(this.renderID);
+      this.renderDiv.innerHTML = '';
+      const svg = document.createElement('svg');
+      this.renderDiv.appendChild(svg);
+      const g = document.createElement('g');
 
     // svg.viewBox = '0 0 500 500';
-    const text = document.createElement('text');
-    text.dominantbaseline = 'text-before-edge';
-    svg.appendChild(g);
-    g.appendChild(text);
-    $(text).attr('x', 10);
-    $(text).attr('y', 10);
-    text.style.fontSize = this.attributes.fontSize;
-    if (this.attributes.columnOptions === null) {
-      this.columnOptions = [];
-    }
-    if (this.attributes.aggs !== undefined) {
-      this.displayCount(text);
+      const text = document.createElement('text');
+      text.dominantbaseline = 'text-before-edge';
+      svg.appendChild(g);
+      g.appendChild(text);
+      $(text).attr('x', 10);
+      $(text).attr('y', 10);
+      text.style.fontSize = this.attributes.fontSize;
+      text.style.fill = this.attributes.color;
+      if (this.attributes.columnOptions === null) {
+        this.columnOptions = [];
+      }
+      if (this.attributes.aggs !== undefined) {
+        this.displayCount(text);
+      }
     }
   }
 
@@ -107,26 +112,27 @@ class Counter extends Visual {
       }
     }
 
-    for (let j = 0; j < this.attributes.aggs.length; j += 1) {
-      if (this.attributes.aggs[j] !== undefined) {
-        const agg = this.attributes.aggs[j];
-        for (let i = 0; i < renderData.length; i += 1) {
-          if (renderData[i] !== null && renderData[i] !== undefined
+    if (this.attributes.aggs !== undefined) {
+      const agg = this.attributes.aggs;
+      for (let i = 0; i < renderData.length; i += 1) {
+        if (renderData[i] !== null && renderData[i] !== undefined
           && !isNaN(renderData[i][agg.column])) {
-            sum += Number(renderData[i][agg.column]);
-          }
-        }
-        if (agg.operation === 'Sum') {
-          text = Counter.createTSpan(svg);
-          text.innerHTML += (`${agg.title} ${Math.round(sum * 100) / 100}`);
-        } else {
-          text = Counter.createTSpan(svg);
-          text.innerHTML += (`${agg.title} ${Math.round((sum / count) * 100) / 100}`);
+          sum += Number(renderData[i][agg.column]);
         }
       }
+      if (agg.operation === 'Sum') {
+        text = Counter.createTSpan(svg);
+        text.innerHTML += (`${agg.title} ${Math.round(sum * 100) / 100}`);
+      }
+      if (agg.operation === 'Average') {
+        text = Counter.createTSpan(svg);
+        text.innerHTML += (`${agg.title} ${Math.round((sum / count) * 100) / 100}`);
+      }
+      if (agg.operation === 'Count') {
+        text = Counter.createTSpan(svg);
+        text.innerHTML += `${agg.title} ${count}`;
+      }
     }
-    text = Counter.createTSpan(svg);
-    text.innerHTML += `Count: ${count}`;
   }
 
   static removeFilter(buttonID) {
@@ -140,30 +146,18 @@ class Counter extends Visual {
     });
   }
 
-  static createAggregationRow(myDiv, cats, num) {
+  static createAggregationRow(myDiv, cats) {
     const editor = new EditorGenerator(myDiv);
-    editor.createAggregationRow(num, 'tspan', cats, 'aggs', (e) => { Counter.removeFilter(e.currentTarget); });
+    editor.createAggregationRow('aggid', 'tspan', cats, 'aggs');
   }
 
-  createAddAggregation(myDiv, aggDiv) {
-    const editor = new EditorGenerator(myDiv);
-    editor.createButton('addAgg', 'New Sum or Average', () => {
-      this.num += 1;
-      Counter.createAggregationRow(aggDiv, this.cats, this.num);
-    });
-  }
 
   updateRender() {
-    const aggRows = document.getElementsByClassName('aggs');
-    for (let i = 0; i < aggRows.length; i += 1) {
-      this.attributes.aggs[i] = {};
-      this.attributes.aggs[i].operation = $(aggRows[i].childNodes[1].children[0].children[3]).val();
-      this.attributes.aggs[i].column = $(aggRows[i].childNodes[3].children[0].children[3]).val();
-      this.attributes.aggs[i].title = $(aggRows[i].childNodes[5].children[0]).val();
-      if (this.attributes.aggs[i].column === null) {
-        this.attributes.aggs[i] = undefined;
-      }
-    }
+    const aggRow = document.getElementById('aggid');
+    this.attributes.aggs = {};
+    this.attributes.aggs.operation = $(aggRow.childNodes[1].children[0].children[3]).val();
+    this.attributes.aggs.column = $(aggRow.childNodes[3].children[0].children[3]).val();
+    this.attributes.aggs.title = $(aggRow.childNodes[5].children[0]).val();
   }
 
   static createTSpan(svg) {
