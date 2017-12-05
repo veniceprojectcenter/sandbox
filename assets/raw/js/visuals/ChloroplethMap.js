d747aa948f7a31707e8ab2cef7b85aae333e1f14
Griffin Bishop <grbishop@wpi.edu>
2017-12-05 12:15:58 +0100

    Min and Max computed for BoundaryInfoObjects

--- assets/raw/js/visuals/ChloroplethMap.js
+++ assets/raw/js/visuals/ChloroplethMap.js
@@ -138,7 +138,10 @@ class ChloroplethMap extends Visual {
       const info = this.getBoundaryInfo(boundary);
       boundaryInfoObjects = boundaryInfoObjects.concat(info);
     });
+
+    const minMax = this.constructor.getInfoObjectsMinMax(boundaryInfoObjects);
     console.log(boundaryInfoObjects);
+    console.log(minMax);
   }
 
   // Returns an array with an object with the average of a given category
@@ -159,7 +162,40 @@ class ChloroplethMap extends Visual {
   }
 
   static getAverageOfField(points, field) {
-    return 5;
+    let sum = 0;
+    let num = 0;
+    points.forEach((point) => {
+      const value = parseFloat(point[field]);
+      if (value !== null && !isNaN(value)) {
+        sum += value;
+        num += 1;
+      }
+    });
+
+    const average = (num === 0) ? null : sum / num;
+    return average;
+  }
+
+  static getInfoObjectsMinMax(infoObjects) {
+    if (infoObjects.length === 0) {
+      return { min: null, max: null }; // Edge case
+    }
+
+    let min = infoObjects[0].average;
+    let max = min;
+    for (let i = 0; i < infoObjects.length; i += 1) {
+      const value = infoObjects[i].average;
+      if (value !== null) {
+        if (value < min) {
+          min = value;
+        }
+        if (value > max) {
+          max = value;
+        }
+      }
+    }
+
+    return { min, max };
   }
 
   renderControls() {
