c0038386f47e29f6f6ae95a396fe8184d959f68b
muggleman1 <31550810+muggleman1@users.noreply.github.com>
2018-12-03 11:44:41 +0100

    Buuble-Chart now pulls directly from attributes.items

--- assets/raw/js/visuals/helpers/Visual.js
+++ assets/raw/js/visuals/helpers/Visual.js
@@ -315,6 +315,26 @@ class Visual {
     }
   }
 
+  /**
+   * Returns an array of data objects with value (amount of items in it), weight (sort order), and
+   * color based on this.attributes.items
+   *
+   * @returns {Object[]}
+   */
+  flattenItems() {
+    const keys = Object.keys(this.attributes.items);
+    if (!keys || keys.length === 0) {
+      return [];
+    }
+    return keys.map((item) => {
+      return { key: item,
+        value: this.attributes.items[item].value,
+        weight: this.attributes.items[item].weight,
+        color: this.attributes.items[item].color,
+      };
+    });
+  }
+
 
   /**
    * Abstract method for rendering controls for the desired visuals, used to render the accordion
